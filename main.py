e0780716dbd0e25f7e7f861fdbef59b91e7e3b87
vanik9900 <parmarkaushikbhai4@gmail.com>
2026-09-09 12:29:51 -0700

    add main file

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="siem",
+    description="System Information And Event Management",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "project": "SentinelAI",
+        "status": "running"
+    }
+
+
+@app.get("/api/health")
+def health():
+    return {
+        "status": "ok"
+    }
